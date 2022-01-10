ec9370fe53c2ee32553aa9b36e93cc9571550fbe
lausek <root@lausek.eu>
2022-01-10 10:37:08 +0100

    improve setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,21 @@ setup(
     name="remarkov",
     version="0.2.0",
     description="Generate text from text using Markov chains.",
-    long_description=open(Path(__file__).parent / "README.md").read(),
-    long_description_content_type="text/markdown",
     author="lausek",
     author_email="input@lausek.eu",
     url="https://github.com/lausek/remarkov",
     packages=find_packages(),
     entry_points={"console_scripts": ["remarkov=remarkov.cli:main"]},
+    long_description=open(Path(__file__).parent / "README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    keywords=[
+        "markov",
+        "cli",
+    ],
 )
